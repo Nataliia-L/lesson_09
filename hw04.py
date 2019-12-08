5f8cb0fc1166a5a960d8e47e5f4b75c256e41e87
Nataliia-L <57313896+Nataliia-L@users.noreply.github.com>
2019-12-08 11:24:46 +0200

    Create hw04.py

---
+++ hw04.py
@@ -0,0 +1,9 @@
+val = input('Please, enter some number. ')
+import string
+import random
+import math
+gener_num = random.randint (1,100)
+if val==string.ascii_letters:
+print ('The value is generated: ', a, math.sqrt(a))
+else:
+print ('The number is: ', val, math.sqrt (val))
